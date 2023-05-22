d4f07202c54c9fbd4dcc6edb931dec8c8a05c54b
Rubens meirelles <rrubens.meirelles@gmail.com>
2023-05-22 20:11:58 -0300

    Criando estrutura de pastas

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ProjectScholl
+# ProjectSchool
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projectScholl'`, () => {
+  it(`should have as title 'ProjectSchool'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projectScholl');
+    expect(app.title).toEqual('ProjectSchool');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('projectScholl app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ProjectSchool app is running!');
   });
 });
