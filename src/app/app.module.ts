639a2b150988acdfbb24230164e752cef621079e
rubens.meirelles <rrubens.meirelles@gmail.com>
2023-05-27 12:50:56 -0300

    Inserindo os componentes de navegação tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './shared/materials/Material/material.module';
+import { MaterialModule } from './shared/materials/material.module';
 
 @NgModule({
   declarations: [
