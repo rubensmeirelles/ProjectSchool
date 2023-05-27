4c1f5a49f49075b86be50ab7ce2fead542dd529f
rubens.meirelles <rrubens.meirelles@gmail.com>
2023-05-27 11:49:26 -0300

    Criando o componente de conquistas

--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { MyConquestsComponent } from './my-conquests/my-conquests.component';
 
 
 @NgModule({
   declarations: [
-    UsersComponent
+    UsersComponent,
+    MyProfileComponent,
+    MyConquestsComponent
   ],
   imports: [
     CommonModule,
