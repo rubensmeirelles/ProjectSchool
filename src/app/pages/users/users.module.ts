d184659463a539e9c84584ac2f31f792580137b2
rubens.meirelles <rrubens.meirelles@gmail.com>
2023-05-27 12:05:05 -0300

    Criando o componente de disciplinas e inserindo as rotas

--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -5,13 +5,15 @@ import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
 import { MyConquestsComponent } from './my-conquests/my-conquests.component';
+import { MyDisciplinesComponent } from './my-disciplines/my-disciplines.component';
 
 
 @NgModule({
   declarations: [
     UsersComponent,
     MyProfileComponent,
-    MyConquestsComponent
+    MyConquestsComponent,
+    MyDisciplinesComponent
   ],
   imports: [
     CommonModule,
