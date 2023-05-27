0198c70a476ae29cf78fd333f46fc9139482a325
rubens.meirelles <rrubens.meirelles@gmail.com>
2023-05-27 14:11:26 -0300

    Criando o componente e módulo de título - toolbar

--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -6,7 +6,8 @@ import { UsersComponent } from './users.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
 import { MyConquestsComponent } from './my-conquests/my-conquests.component';
 import { MyDisciplinesComponent } from './my-disciplines/my-disciplines.component';
-import { UsersMaterialModule } from 'src/app/shared/materials/users-mat.module';
+import { UsersMaterialModule } from '@app/shared/materials/users-mat.module';
+import { SharedModule } from '@app/shared/shared.module';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { UsersMaterialModule } from 'src/app/shared/materials/users-mat.module';
   imports: [
     CommonModule,
     UsersMaterialModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    SharedModule
   ]
 })
 export class UsersModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { ToolbarTitleComponent } from './components/toolbar-title/toolbar-title.component';
+
+@NgModule({
+  declarations: [
+    ToolbarTitleComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    ToolbarTitleComponent
+  ]
+})
+export class SharedModule { }
