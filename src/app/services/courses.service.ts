7936dd3d1862f130b74bc93804d76fec7c5e0497
rubens.meirelles <rrubens.meirelles@gmail.com>
2023-10-03 21:35:57 -0300

    Criando os endpoints

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -11,7 +11,23 @@ export class CoursesService {
   baseUrl = `${environment.apiUrl}courses`;
   private http = inject(HttpClient);
 
-  public getCoursesById(id: number): Observable<Course[]> {
+  public get(): Observable<Course[]> {
+    return this.http.get<Course[]>(`$(this.baseUrl}`)
+  }
+
+  public getById(id: number): Observable<Course[]> {
     return this.http.get<Course[]>(`$(this.baseUrl}/${id}`)
+  }
+
+  public post(course: Course): Observable<Course[]> {
+    return this.http.post<Course[]>(`$(this.baseUrl}`, course)
+  }
+
+  public put(id: number, course: Course): Observable<Course[]> {
+    return this.http.put<Course[]>(`$(this.baseUrl}/${id}`, course)
+  }
+
+  public delete(id: number): Observable<Course[]> {
+    return this.http.delete<Course[]>(`$(this.baseUrl}/${id}`)
   }
 }
